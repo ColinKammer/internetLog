af1418bcf1b7a147b1ee4dd530b35530dd9a5164
ColinKammer <colin.justus.kammer qiem.thm.de>
2020-12-02 17:39:49 +0100

    Implement Parsing for Ping, Refactor

--- evaluateLib.py
+++ evaluateLib.py
@@ -4,17 +4,46 @@ REGEX_PATTERN_TIMESTAMP = '^\/\/#! Time: ([\d|.| |:]*)'
 REGEX_PATTERN_TEST = '\/\/## ([A-z]*):[ ]*[.|/]*([A-z]*)'
 
 
-class Datapoint:
+class Datapoint(object):
     def __init__(self, timestamp, tests):
         self.Timestamp = timestamp
         self.Tests = tests
 
 
-class Test:
-    def __init__(self, name, coreCommand, measuredValues):
+class Test(object):
+    def __init__(self, name):
         self.Name = name
-        self.CoreCommand = coreCommand
-        self.MeasuredValues = measuredValues
+
+
+class SpeedTest(Test):
+    CoreCommand = "speedtest"
+
+    def __init__(self, name, tstString):
+        super().__init__(name)
+
+        self.DownloadMbits = float(
+            re.search('Download: (\d*\.?\d*) Mbit/s', tstString).group(1))
+        self.UploadMbits = float(
+            re.search('Upload: (\d*\.?\d*) Mbit/s', tstString).group(1))
+
+class PingTest(Test):
+    CoreCommand = "ping"
+
+    def __init__(self, name, tstString):
+        super().__init__(name)
+
+
+        self.PercentPacketLoss = int(
+            re.search('received, (\d*)% packet loss', tstString).group(1))
+        if(self.PercentPacketLoss != 100):
+            self.PingAvgMs = float(
+                re.search('rtt min\/avg\/max\/mdev = \d*\.?\d*\/(\d*\.?\d*)\/\d*', tstString).group(1))
+        else:
+            self.PingAvgMs = 10000.0 #Dummy Value for ping unsucessfull
+
+availableTestTypes = {}
+availableTestTypes[SpeedTest.CoreCommand] = lambda n, s: SpeedTest(n, s)
+availableTestTypes[PingTest.CoreCommand] = lambda n, s: PingTest(n, s)
 
 
 def findSubstringsStartingWith(string, partStartSequence):
@@ -40,7 +69,12 @@ def parseTest(tstString):
     name = re.search(REGEX_PATTERN_TEST, tstString).group(1)
     coreCommand = re.search(REGEX_PATTERN_TEST, tstString).group(2)
 
-    return Test(name, coreCommand, [])
+    if(coreCommand not in availableTestTypes):
+        print("No test type available for coreCommand " + coreCommand)
+        return None
+
+    testType = availableTestTypes[coreCommand]
+    return testType(name, tstString)
 
 
 def parseDatapoint(dpString):
